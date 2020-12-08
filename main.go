29c49496187570670feba170366971fd88384f68
Funny-Systems-OSS <oss@funny.systems>
2020-12-08 12:20:43 +0800

    make it better

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package funny
 
 import (
     "log"
+    "bytes"
     "crypto/aes"
 	"crypto/cipher"
     "crypto/md5"
@@ -20,13 +21,13 @@ const Funny = `
                                                 \|___|/
 `
 
-func md5sum(text string) string {
+func Md5sum(text string) string {
     hash := md5.Sum([]byte(text))
     return hex.EncodeToString(hash[:])
 }
 
 func KeyGenerator(val int) string {
-    return md5sum(strconv.Itoa(val))[:32]
+    return Md5sum(strconv.Itoa(val))[:32]
 }
 
 func NonceGenerator(val int) string {
@@ -49,4 +50,22 @@ func Decrypt(ciphertext, key, nonce []byte) (plaintext []byte) {
         log.Fatal(err)
     }
     return
+}
+
+func Encrypt(plaintext, key, nonce []byte) []byte {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    aesgcm, err := cipher.NewGCM(block)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return aesgcm.Seal(nil, nonce, plaintext, nil)
+}
+
+func Validate(data1 []byte, data2 []byte) bool {
+    return bytes.Equal(data1, data2)
 }
